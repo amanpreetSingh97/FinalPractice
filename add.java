2b1c0aa429c6050bd202e427c97bbd549a51d40c
amanpreetSingh97 <honeysingh.hs490@gmail.com>
2019-09-28 17:18:46 +0530

    final add

--- add.java
+++ add.java
@@ -1,6 +1,7 @@
 public class add {
 	public static void main(String args[]) {
-
-		return Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+                int a = Integer.parseInt(args[0]); 
+                int b = Integer.parseInt(args[1]);
+		System.out.println(a+b);
 	}
 }
